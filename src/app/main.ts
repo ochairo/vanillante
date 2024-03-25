c4d9e4920bfe2e0cc121089308c1d2d75eac93e2
ochairo <19258508+ochairo@users.noreply.github.com>
2024-03-25 16:09:34 +0900

    Update main.ts

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,9 +2,7 @@ import { router } from "./infrastructure/router/router";
 import { routesConfig } from "./routes";
 
 const rootElement = document.getElementById("root")!;
-(() => {
-  window.addEventListener("popstate", () => router(routesConfig, rootElement));
-  document.addEventListener("DOMContentLoaded", () =>
-    router(routesConfig, rootElement)
-  );
-})();
+window.addEventListener("popstate", () => router(routesConfig, rootElement));
+document.addEventListener("DOMContentLoaded", () =>
+  router(routesConfig, rootElement)
+);
